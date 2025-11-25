8ebbf84ab74b25ba1db68a3830f52d89c0047772
JessieJ <jvwiltshire22@gmail.com>
2025-11-25 11:15:53 +0000

    feat: include related travel posts in Country show response
    - updated GET /country/:countryId to query TravelPost collection for matching entries
    - return combined payload containing the country and its related posts
    - ensured not-found handling and response structure remain consistent

--- controllers/country.js
+++ controllers/country.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import Country from '../models/country.js'
 import { NotFound } from '../utils/errors.js'
+import TravelPost from '../models/travelPost.js'
 
 
 
@@ -25,10 +26,16 @@ router.get('', async (req, res, next) => {
 router.get('/:countryId', async (req, res, next) => {
 try {
     const {countryId} = req.params
-    const country = await Country.findById(countryId).populate("TravelPost")
+    const country = await Country.findById(countryId)
 
     if(!country) throw new NotFound('Country not found')
-    res.status(200).json(country)
+
+    const relatedPosts = await TravelPost.find({ country: country._id })
+
+    res.status(200).json({
+        country: country,
+        relatedPosts: relatedPosts
+    })
     console.log("COUNTRY HAS BEEN LOCATED")
 } catch (error) {
     next(error)
